1d7d7ef07e5f2014e0f98bccd77b425adc5b07cd
FQ.W <fengqiu.wu@shuyun.com>
2019-06-09 21:50:46 +0800

    :bug:修复通过菜单切换文档时scrollTop没重置问题

--- demos/components/markdown/index.js
+++ demos/components/markdown/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './index.less';
 
 export default function MarkdownOutput({ title, html, subtitle, className, ...props }) {
+	const markdownRef = useRef();
+
+	// reset scrollTop
+	useEffect(() => {
+		markdownRef.current.parentNode.scrollTop = 0;
+	}, []);
+
 	return (
-		<section className={classes.markdownBody} {...props}>
+		<section ref={markdownRef} className={classes.markdownBody} {...props}>
 			<h1 className={classes.title}>{title} {subtitle}</h1>
 			<div dangerouslySetInnerHTML={{ __html: html }} />
 		</section>
