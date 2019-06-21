82f6dac94ab13b113b6df753f85daf725d45f224
rookie <964379494@qq.com>
2019-06-22 01:48:19 +0800

    :bug: 处理文档的样式会污染code-demo的样式问题

--- demos/components/markdown/index.js
+++ demos/components/markdown/index.js
@@ -4,21 +4,52 @@ import PropTypes from 'prop-types';
 import CodeBox from '../code-box';
 import classes from './index.less';
 
+const createElement = (className = '__empty__') => {
+	const element = document.createElement('div');
+	element.classList.add(className);
+	return element;
+}
+
+const separateMarkdownHtml = html => {
+	const container = createElement();
+	const tmpWrap = createElement();
+	tmpWrap.innerHTML = html;
+
+	let markdownBody = createElement(classes.markdownBody);
+
+	[...tmpWrap.children].forEach(element => {
+		if (!element.id || element.id !== 'code-demo') {
+			markdownBody.appendChild(element);
+		} else {
+			// append markdownBody
+			container.appendChild(markdownBody);
+
+			// append code-demo Element
+			container.appendChild(element);
+
+			// create a new markdownBody
+			markdownBody = createElement(classes.markdownBody);
+		}
+	});
+
+	container.appendChild(markdownBody);
+
+	return container.innerHTML;
+}
+
 export default class MarkdownOutput extends React.Component {
 	static propTypes = {
 		demos: PropTypes.any,
 		title: PropTypes.string,
 		html: PropTypes.string,
-		subtitle: PropTypes.string,
-		className: PropTypes.string
+		subtitle: PropTypes.string
 	};
 
 	static defaultProps = {
 		demos: [],
 		title: '',
 		html: '',
-		subtitle: '',
-		className: ''
+		subtitle: ''
 	};
 
 	constructor(props) {
@@ -55,14 +86,12 @@ export default class MarkdownOutput extends React.Component {
 	}
 
 	render() {
-		const { title, html, subtitle, className, ...props } = this.props;
+		const { title, html, subtitle } = this.props;
 
 		return (
-			<section ref={this.markdownRef} {...props}>
+			<section ref={this.markdownRef}>
 				<h1 className={classes.title}>{title} {subtitle}</h1>
-				<div
-					className={classes.markdownBody}
-					dangerouslySetInnerHTML={{ __html: html }} />
+				<div dangerouslySetInnerHTML={{ __html: separateMarkdownHtml(html) }} />
 			</section>
 		);
 	}
