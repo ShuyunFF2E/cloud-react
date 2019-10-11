edd9e8315f16b61e96c0e234274b55c8c183403c
闰楠 <runrun@nanrundeMacBook-Pro.local>
2019-10-11 14:24:17 +0800

    review问题修改

---
+++ src/components/tooltip/config.js
@@ -0,0 +1,14 @@
+// Tooltip配置
+export const CONFIG_PLACE = {
+    top: 'top',
+    left: 'left',
+    right: 'right',
+    bottom: 'bottom',
+    center: 'center',
+    auto: 'auto'
+}; 
+export const CONFIG_THEME = {
+    dark: 'dark',
+    error: 'error',
+    light: 'light'
+}
