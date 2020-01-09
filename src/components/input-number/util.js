11f8d97a9d1e4d9842cb3aae5fa2df76d461611e
seawind8888 <344023858@qq.com>
2020-01-09 17:35:14 +0800

    🐛 [InputNumber组件] 数字类型默认为不限 #127

--- src/components/input-number/util.js
+++ src/components/input-number/util.js
@@ -2,11 +2,21 @@ export function isInvalid(value) {
 	return Number.isNaN(Number(value)) || value === null || value.toString().trim() === '';
 }
 
-export function getCurrentValue(value, min, max, precision) {
+/**
+ *
+ *
+ * @export
+ * @param {*} value 传入的值
+ * @param {*} min 最大
+ * @param {*} max 最小
+ * @param {boolean} [precision=false] 精度，false为无限
+ * @returns
+ */
+export function getCurrentValue(value, min, max, precision = -1) {
 	if (isInvalid(value)) {
 		return '';
 	}
-	const val = Number(value).toFixed(Math.abs(parseInt(precision, 10)));
+	const val = precision >= 0 ? Number(value).toFixed(Math.abs(parseInt(precision, 10))) : Number(value);
 	if (val > max) {
 		return max;
 	}
@@ -49,4 +59,30 @@ export function getValueByBlank(min, max, step) {
 		return min;
 	}
 	return 0;
+}
+
+/**
+ *
+ * 解决js精度丢失
+ * @export
+ * @param {*} n1
+ * @param {*} n2
+ * @param {string} [type='add'] 运算类型，默认加法(可扩展)
+ * @returns
+ */
+export function fixDoubleOperation(n1, n2, type = 'add') {
+	const l1 = String(n1).indexOf('.') >= 0 ? String(n1).split('.')[1].length : 0
+	const l2 = String(n2).indexOf('.') >= 0 ? String(n2).split('.')[1].length : 0
+	const displacement = 10 ** Math.max(l1,l2)
+	const _n1 = n1 * displacement
+	const _n2 = n2 * displacement
+	switch (type) {
+		case 'add': {
+			return (_n1 + _n2) / displacement
+		}
+		default: {
+			return (_n1 + _n2) / displacement
+		}
+	}
+	
 }
