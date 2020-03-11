d72243668cf79d6c070cae79242086b27018ada6
fang.yang <yangfang2028@126.com>
2020-03-11 16:06:41 +0800

    :lipstick: :sparkles: [Toggle] 增加onBeforeChange支持，css优化

--- src/components/toggle/demos/size.markdown
+++ src/components/toggle/demos/size.markdown
@@ -4,47 +4,46 @@ title: toggle 大小 size 使用
 desc: 不配置size即为默认的 default 大小，设置size = samll 为小号toggle
 ---
 
-````javascript
+```javascript
 import React from 'react';
 import { Button, Toggle } from 'cloud-react';
 
 export default class ToggleSizeDemo extends React.Component {
-
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			checked: true,
 			size: 'default'
-		}
+		};
 	}
 
-	handleChange = (checked) => {
+	handleChange = checked => {
 		this.setState({ checked });
-	}
+	};
 
-	handleSize = (size) => {
-		this.setState({ size })
-	}
+	handleSize = size => {
+		this.setState({ size });
+	};
 
 	render() {
-
 		const { size, checked } = this.state;
 
 		return (
 			<div>
 				当前size为<span className="hightlight">{size}</span>：
-				<Toggle size={size} checked={checked} onChange={this.handleChange} />
-
-				<Button size="small" type="normal" className="item" onClick={this.handleSize.bind(this, 'default')}>切换为default</Button>
-
-				<Button size="small" type="primary" onClick={this.handleSize.bind(this, 'small')}>切换为small</Button>
+				<Toggle size={size} checked={checked} onChange={this.handleChange} checkedText="开" unCheckedText="关" />
+				<Button size="small" type="normal" className="item" onClick={this.handleSize.bind(this, 'default')}>
+					切换为default
+				</Button>
+				<Button size="small" type="primary" onClick={this.handleSize.bind(this, 'small')}>
+					切换为small
+				</Button>
 			</div>
 		);
 	}
 }
-
-````
+```
 
 ````less
 .hightlight {
@@ -54,3 +53,4 @@ export default class ToggleSizeDemo extends React.Component {
 	margin: 0 10px;
 }
 ```
+````
--- src/components/toggle/index.js
+++ src/components/toggle/index.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { prefixCls } from '@utils/config';
 import './index.less';
 
-class Toggle extends Component {
+const Toggle = props => {
+	const { checked, checkedText, unCheckedText, size, disabled, onChange, onBeforeChange } = props;
 
-	handleClick = (event) => {
+	const prefix = `${prefixCls}-toggle`;
 
-		const { checked, disabled, onChange } = this.props;
+	const wrapper = classNames(prefix, {
+		[`${prefix}-small`]: size === 'small',
+		[`${prefix}-checked`]: checked,
+		[`${prefix}-disabled`]: disabled
+	});
 
+	const handleClick = event => {
 		if (disabled) {
+			return;
+		}
+
+		if (onBeforeChange) {
+			onBeforeChange();
 			return;
 		}
 
 		if (onChange) {
 			onChange(!checked, event);
 		}
-	}
+	};
 
-	render() {
-
-		const { checked, checkedText, unCheckedText, size, disabled } = this.props;
-
-		const wrapper = classNames(`${prefixCls}-toggle`, {
-			[`${prefixCls}-toggle-small`]: size === 'small',
-			[`${prefixCls}-toggle-checked`]: checked,
-			[`${prefixCls}-toggle-disabled`]: disabled
-		});
-
-		return (
-			<button type="button" className={wrapper} onClick={this.handleClick}>
-				<span className={`${checked}-toggle-inner`}>{ checked ? checkedText : unCheckedText }</span>
-			</button>
-		);
-	}
-}
+	return (
+		<button type="button" className={wrapper} onClick={handleClick}>
+			<span className={`${prefix}-inner`}>{checked ? checkedText : unCheckedText}</span>
+		</button>
+	);
+};
 
 Toggle.propTypes = {
 	size: PropTypes.oneOf(['default', 'small']),
