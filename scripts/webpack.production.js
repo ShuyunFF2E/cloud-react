7e5ed105f05dcacfe1e932f66fd0a5fe8b2cba35
baukh789 <182209508@qq.com>
2020-05-26 10:10:38 +0800

    :sparkles: 打包输出umd命名改造，支持script方式

--- scripts/webpack.production.js
+++ scripts/webpack.production.js
@@ -18,7 +18,14 @@ module.exports = () => ({
 	output: {
 		filename: '[name].js',
 		path: buildOutputDir,
-		libraryTarget: 'umd'
+		library: 'CloudReact',
+		libraryTarget: 'umd',
+		auxiliaryComment: {
+			root: 'CloudReact',
+			commonjs: 'cloud-react',
+			commonjs2: 'cloud-react',
+			amd: 'cloud-react'
+		}
 	},
 	module: {
 		rules: [
@@ -30,8 +37,18 @@ module.exports = () => ({
 		]
 	},
 	externals: {
-		react: 'react',
-		'react-dom': 'react-dom'
+		react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom'
+        }
 	},
 	performance: {
 		hints: false
