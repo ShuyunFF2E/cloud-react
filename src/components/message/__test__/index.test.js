819e8116435d9e33a897ea792256802cf6025d61
fang.yang <yangfang2028@126.com>
2020-07-01 19:46:17 +0800

    ✅ [Message] 单元测试编写

---
+++ src/components/message/__test__/index.test.js
@@ -0,0 +1,60 @@
+import { prefixCls } from '@utils';
+
+import Message from '../index';
+
+const selector = `.${prefixCls}-message`;
+
+describe('Message', () => {
+	beforeEach(() => {
+		jest.useFakeTimers();
+	});
+
+	afterEach(() => {
+		jest.useRealTimers();
+	});
+
+	it('should renders correctly', () => {
+		Message.success('sucess info');
+		expect(document.querySelectorAll(`${selector}-success`)).toHaveLength(1);
+
+		Message.error('error info');
+		expect(document.querySelectorAll(`${selector}-error`)).toHaveLength(1);
+		document.querySelectorAll(`${selector}-error`);
+
+		jest.runAllTimers();
+	});
+
+	it('should auto close after duration', () => {
+		Message.success('sucess info', { duration: 1000 });
+
+		expect(document.querySelectorAll(`${selector}-success`)).toHaveLength(1);
+
+		jest.runAllTimers();
+
+		expect(document.querySelectorAll(`${selector}-success`)).toHaveLength(0);
+	});
+
+	it('should render to verb container', () => {
+		const element = document.createElement('div');
+		element.setAttribute('id', 'container');
+		document.body.appendChild(element);
+
+		const container = document.getElementById('container');
+
+		Message.success('sucess info', { duration: 1000, contextContainer: container });
+
+		expect(container.querySelectorAll(`${selector}-success`)).toHaveLength(1);
+
+		jest.runAllTimers();
+	});
+
+	it('should destory when click close icon', () => {
+		Message.success('sucess info', { duration: 0 });
+
+		expect(document.querySelectorAll(`${selector}-success`)).toHaveLength(1);
+
+		document.querySelectorAll('.close-icon')[0].click();
+
+		expect(document.querySelectorAll(`${selector}-success`)).toHaveLength(0);
+	});
+});
