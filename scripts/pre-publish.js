c7610cb26a3f699f1ad40c067d8c5b8cb6d3189f
seawind8888 <344023858@qq.com>
2021-11-16 18:24:31 +0800

    scripts: 增加dist/cloud-react.css

--- scripts/pre-publish.js
+++ scripts/pre-publish.js
@@ -8,8 +8,12 @@ function start() {
     const distPackPath = path.resolve('dist/package.json')
     const woffFile = fs.readFileSync(path.resolve('src/components/icon/fonts/iconfont.woff'), 'utf-8')
     const base64 = Buffer.from(woffFile).toString('base64')
-    fs.writeFileSync(distCSSFilePath, fs.readFileSync(distCSSFilePath, 'utf-8').replace('./fonts/iconfont.woff', `data:font/woff;base64,${base64}`), 'utf-8')
+    const _cssFile = fs.readFileSync(distCSSFilePath, 'utf-8').replace('./fonts/iconfont.woff', `data:font/woff;base64,${base64}`)
+    fs.writeFileSync(distCSSFilePath, _cssFile, 'utf-8')
     fs.writeFileSync(distPackPath, fs.readFileSync(distPackPath, 'utf-8').replace('dist/cloud-react.js', 'cloud-react.js'), 'utf-8')
+    fs.mkdirSync(path.resolve('dist/dist'))
+    fs.writeFileSync(path.resolve('dist/dist/cloud-react.css'), _cssFile, 'utf-8')
+
 }
 
 
