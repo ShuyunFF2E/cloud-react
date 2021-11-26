c8f98a3dbae46eb375a4d174dc1361689b3fb326
liyuan-meng <13149260506@163.com>
2021-11-26 14:57:07 +0800

    :sparkles: 添加 Popover 组件

---
+++ src/components/popover/index.js
@@ -0,0 +1,108 @@
+import React, { createRef } from 'react';
+import PropTypes from 'prop-types';
+import { noop, prefixCls } from '@utils';
+import Tooltip from '../tooltip';
+import Icon from '../icon';
+import Button from '../button';
+
+import './index.less';
+
+const classSelector = `${prefixCls}-popover`;
+const confirmBtnClass = `${classSelector}-confirm`;
+const cancelBtnClass = `${classSelector}-cancel`;
+
+function Popover(props) {
+	const { title, content, showIcon, iconTpl, showCancelBtn, showConfirmBtn, cancelBtnText, confirmText, width, children, ...otherProps } = props;
+
+	const ref = createRef();
+
+	const closeTooltipExec = evtPath => {
+		return evtPath.find(ele => {
+			return ele.classList && ele.classList.contains && (ele.classList.contains(cancelBtnClass) || ele.classList.contains(confirmBtnClass));
+		});
+	};
+
+	const handleCancelClick = () => {
+		props.onCancelClick();
+	};
+
+	const handleConfirmClick = async () => {
+		const removeClass = () => {
+			const ele = ref.current.querySelector(`.${classSelector}-confirm`);
+			if (ele) {
+				ele.classList.remove(confirmBtnClass);
+			}
+		};
+
+		try {
+			const isInvalidate = await props.onConfirmClick();
+			if (isInvalidate) {
+				removeClass();
+			}
+		} catch {
+			removeClass();
+		}
+	};
+
+	const popoverContent = (
+		<div className={classSelector}>
+			<section className={`${classSelector}-content`}>
+				{showIcon && (iconTpl || <Icon type="warning-circle-solid" />)}
+
+				<div className={`${classSelector}-main-content`}>
+					{title && <p className={`${classSelector}-title`}>{title}</p>}
+					{content && <p className={`${classSelector}-desc`}>{content}</p>}
+				</div>
+			</section>
+
+			<section className={`${classSelector}-btn`} ref={ref}>
+				{showCancelBtn && (
+					<Button onClick={handleCancelClick} className={cancelBtnClass}>
+						{cancelBtnText}
+					</Button>
+				)}
+				{showConfirmBtn && (
+					<Button type="primary" onClick={handleConfirmClick} className={confirmBtnClass}>
+						{confirmText}
+					</Button>
+				)}
+			</section>
+		</div>
+	);
+
+	return (
+		<Tooltip closeTooltipExec={closeTooltipExec} content={popoverContent} theme="light" overlayStyle={{ width, maxWidth: width }} {...otherProps}>
+			{children}
+		</Tooltip>
+	);
+}
+
+Popover.propTypes = {
+	width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+	title: PropTypes.string,
+	content: PropTypes.any,
+	showIcon: PropTypes.bool,
+	iconTpl: PropTypes.any,
+	showCancelBtn: PropTypes.bool,
+	showConfirmBtn: PropTypes.bool,
+	cancelBtnText: PropTypes.string,
+	confirmText: PropTypes.string,
+	onCancelClick: PropTypes.func,
+	onConfirmClick: PropTypes.func
+};
+
+Popover.defaultProps = {
+	width: 245,
+	title: '',
+	content: '',
+	showIcon: false,
+	iconTpl: '',
+	showCancelBtn: false,
+	showConfirmBtn: false,
+	cancelBtnText: '取消',
+	confirmText: '确认',
+	onCancelClick: noop,
+	onConfirmClick: noop
+};
+
+export default Popover;
