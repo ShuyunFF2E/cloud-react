8cc42803e608e88a19c74805fc57149d12b12b9a
crystal <492150523@qq.com>
2021-12-08 18:16:00 +0800

    [select]修复复杂option搜索问题

--- src/components/select/utils.js
+++ src/components/select/utils.js
@@ -1,33 +1,42 @@
 import ShuyunUtils from 'shuyun-utils';
 
 // 将组件children转换成带label|value的对象数组
-export const formatOptionSource = data => {
-	const result = [];
-	if (ShuyunUtils.type(data) === 'object') {
-		result.push({
-			label: data.children,
-			value: data.value
-		});
-	}
-	return result;
+export const formatOptionSource = (data) => {
+  const result = [];
+  if (ShuyunUtils.type(data) === 'object') {
+    result.push({
+      label: data.children,
+      value: data.value,
+    });
+  }
+  return result;
 };
 
 export const filterOptions = (options, filter) => {
-	return options.reduce((acc, child) => {
-		const { children } = child.props;
-		const label = Array.isArray(children) ? children.find(v => typeof v === 'string' || typeof v === 'number') : children;
-		if (label && String(label).indexOf(filter) > -1) {
-			acc.push(child);
-		}
-		return acc;
-	}, []);
+  return options.reduce((acc, child) => {
+    const { children } = child.props;
+    const label = Array.isArray(children)
+      ? children.find((v) => typeof v === 'string' || typeof v === 'number')
+      : children;
+    if (label && String(label).indexOf(filter) > -1) {
+      acc.push(child);
+    }
+    return acc;
+  }, []);
 };
 
 export const getSearchTagName = (name, searchValue) => {
+  if (typeof name !== 'string') {
+    return name;
+  }
+
   if ([null, undefined, ''].includes(searchValue)) {
     return name;
   }
 
   const reg = new RegExp(searchValue, 'g');
-  return name.replace(reg, `<span style="color: #8FAFFF; padding: 0;">${searchValue}</span>`);
+  return name.replace(
+    reg,
+    `<span style="color: #8FAFFF; padding: 0;">${searchValue}</span>`,
+  );
 };
