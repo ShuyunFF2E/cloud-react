d3c8eec1a5aab4b96f04f05fb3dc9b524e612359
sevenflow <wentao.yu@shuyun.com>
2022-05-31 11:06:38 +0800

    rebase: 合并代码

--- src/components/step/index.js
+++ src/components/step/index.js
@@ -4,68 +4,74 @@ import classnames from 'classnames';
 import { prefixCls, noop } from '@utils';
 import StepItem from './item';
 
-import { HORIZONTAL, VERTICAL, INLINE, CIRCLE, DOT, PROCESS, WAIT, FINISH, DEFAULT, SMALL } from './constants';
+import {
+  HORIZONTAL, VERTICAL, INLINE, CIRCLE, DOT, PROCESS, WAIT, FINISH, DEFAULT, SMALL,
+} from './constants';
 
 import './index.less';
 
 export default class Step extends PureComponent {
-	static propTypes = {
-		current: PropTypes.number,
-		direction: PropTypes.oneOf([HORIZONTAL, VERTICAL, INLINE]),
-		type: PropTypes.oneOf([CIRCLE, DOT]),
-		size: PropTypes.oneOf([DEFAULT, SMALL]),
-		children: PropTypes.any,
-		onClick: PropTypes.func,
-		className: PropTypes.string
-	};
+    static propTypes = {
+      current: PropTypes.number,
+      direction: PropTypes.oneOf([ HORIZONTAL, VERTICAL, INLINE ]),
+      type: PropTypes.oneOf([ CIRCLE, DOT ]),
+      size: PropTypes.oneOf([ DEFAULT, SMALL ]),
+      children: PropTypes.any,
+      onClick: PropTypes.func,
+      className: PropTypes.string,
+    };
 
-	static defaultProps = {
-		current: 0,
-		direction: HORIZONTAL,
-		type: CIRCLE,
-		children: null,
-		className: '',
-		onClick: noop
-	};
+    static defaultProps = {
+      current: 0,
+      direction: HORIZONTAL,
+      type: CIRCLE,
+      children: null,
+      className: '',
+      onClick: noop,
+    };
 
-	static Item = StepItem;
+    static Item = StepItem;
 
-	getStatus(index) {
-		const { current } = this.props;
+    getStatus(index) {
+      const { current } = this.props;
 
-		if (current === index) {
-			return PROCESS;
-		}
+      if (current === index) {
+        return PROCESS;
+      }
 
-		if (index < current) {
-			return FINISH;
-		}
+      if (index < current) {
+        return FINISH;
+      }
 
-		return WAIT;
-	}
+      return WAIT;
+    }
 
-	render() {
-		const { direction, type, size, children, className, onClick: rootClick, ...stepProps } = this.props;
-		const classNames = classnames(`${prefixCls}-step`, direction, type, className, size);
-		const elements = Children.map(children, (Child, index) => {
-			const { status, content, onClick, ...props } = Child.props;
+    render() {
+      const {
+        direction, type, size, children, className, onClick: rootClick, ...stepProps
+      } = this.props;
+      const classNames = classnames(`${prefixCls}-step`, direction, type, className, size);
+      const elements = Children.map(children, (Child, index) => {
+        const {
+          status, content, onClick, ...props
+        } = Child.props;
 
-			return cloneElement(Child, {
-				...props,
-				index,
-				status: status || this.getStatus(index),
-				onClick: () => {
-					onClick(index);
-					rootClick(index);
-				},
-				content: direction === HORIZONTAL ? null : content
-			});
-		});
+        return cloneElement(Child, {
+          ...props,
+          index,
+          status: status || this.getStatus(index),
+          onClick: () => {
+            onClick(index);
+            rootClick(index);
+          },
+          content: direction === HORIZONTAL ? null : content,
+        });
+      });
 
-		return (
-			<div className={classNames} {...stepProps}>
-				{elements}
-			</div>
-		);
-	}
+      return (
+        <div className={classNames} {...stepProps}>
+          {elements}
+        </div>
+      );
+    }
 }
