718d08de3a9afd067eff80367c5e943617dfb939
hanwu <han.wu@shuyun.com>
2023-03-15 18:07:25 +0800

    重制father更改

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -25,9 +25,8 @@ const isBuild = !(argv.dev || argv.watch);
 const externals = [
   'react',
   'react-dom',
-  // ...(isBuild ? [] : ['gridmanager-react', 'prop-types', 'classnames']),
+  ...(isBuild ? [] : ['gridmanager-react', 'prop-types', 'classnames']),
 ];
-console.log(isBuild);
 
 export default isBuild ? [{
   esm: isBuild ? false : 'rollup',
@@ -291,57 +290,6 @@ export default isBuild ? [{
     },
   },
   extraRollupPlugins: [
-    generatePackageJson({
-      outputFolder: 'dist',
-      baseContents: (pkg) =>
-        isBuild
-          ? {
-              name,
-              version,
-              description,
-              main,
-              dependencies,
-              devDependencies,
-            }
-          : {
-              name,
-              version,
-              description,
-							scripts,
-							dumiAssets,
-              main,
-              module,
-              source,
-              jest,
-              dependencies,
-              devDependencies,
-            },
-      additionalDependencies: {
-				"classnames": "^2.3.1",
-				"gray-matter": "^4.0.3",
-				"gridmanager-react": "1.10.2-0",
-				"less": "^3.13.1",
-				"moment": "^2.29.1",
-				"prop-types": "^15.7.2",
-				"rc-cascader": "^3.4.2",
-				"rc-color-picker": "^1.2.6",
-				"rc-drawer": "^6.0.1",
-				"rc-dropdown": "~4.0.1",
-				"rc-menu": "^9.0.12",
-				"rc-picker": "^2.5.19",
-				"rc-table": "7.28.1",
-				"rc-tooltip": "~5.2.0",
-				"rc-trigger": "^5.2.10",
-				"rc-util": "^5.21.5",
-				"react-drag-listview": "^0.2.0",
-				"react-resizable": "^3.0.4",
-				"react-transition-group": "^4.4.2",
-				"rollup-plugin-copy": "^3.4.0",
-				"rollup-plugin-eslint": "^7.0.0",
-				"rollup-plugin-generate-package-json": "^3.2.0",
-				"shuyun-utils": "0.0.3"
-      },
-    }),
     eslint({
       throwOnError: true,
       include: [NodePath.resolve('src/components')],
@@ -353,7 +301,7 @@ export default isBuild ? [{
     // }),
     copy({
       targets: [
-        // { src: 'package.json', dest: 'dist' },
+        { src: 'package.json', dest: 'dist' },
         { src: 'src/locale', dest: 'dist' },
       ],
     }),
