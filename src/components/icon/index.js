080aead6d09523c056db87a31512228e49cdac10
cjmafei <1135328499@qq.com>
2023-05-24 11:09:14 +0800

    fix:[Icon]引入字体库时机修改 (#702)

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { prefixCls, loadScript } from '@utils';
 import './index.less';
 
-loadScript(
-  'https://cloud-cdn.shuyun.com/cloud-fe-iconfont/0.0.2/cloud-react/color/symbol.min.js',
-);
 
-const iconPrefixCls = prefixCls === 'cloud' ? 'icon' : `${prefixCls}-icon`;
 const Icon = ({
   type, style, className, ...restProps
-}) => (
-  <i
-    className={classNames(
-      className,
-      `${prefixCls}-icon ${iconPrefixCls}-${type}`,
-    )}
-    style={style}
-    {...restProps}
-  />
-);
+}) => {
+  useEffect(() => {
+    loadScript(
+      'https://cloud-cdn.shuyun.com/fe-publish-asset/0.0.37/libs/cloud-react/icon/color/symbol.min.js',
+    );
+  }, []);
+  return (
+    <i
+      className={classNames(className, `${prefixCls}-icon icon-${type}`)}
+      style={style}
+      {...restProps}
+    />
+  );
+};
 
 const ColorIcon = ({
   type, style, className, ...restProps
