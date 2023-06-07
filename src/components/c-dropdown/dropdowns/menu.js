b7a806048bc76fa72a08db89641d225584994e22
joylee <jie.li@shuyun.com>
2023-06-07 16:04:04 +0800

    feat: Add dropdown button component.

---
+++ src/components/c-dropdown/dropdowns/menu.js
@@ -0,0 +1,25 @@
+import React, { cloneElement, Children } from 'react';
+import classNames from 'classnames';
+import { prefixCls } from '@utils';
+import 'rc-dropdown/assets/index.css';
+
+function DropdownMenu(props) {
+  const { children, className, restProps } = props;
+
+  return (
+    <div
+      className={classNames(`${prefixCls}-dropdowns-menu`, className)}
+      {...restProps}
+    >
+      {Children.map(children, (child) => cloneElement(child, {
+        onClick(key) {
+          if (typeof props.onClick === 'function') {
+            props.onClick(key);
+          }
+        },
+      }))}
+    </div>
+  );
+}
+
+export default DropdownMenu;
