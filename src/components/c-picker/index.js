cdc9bd4635e0e758b51fc6ef9ac629537ebf97a5
hanwu <han.wu@shuyun.com>
2023-07-06 14:35:54 +0800

    删除moment

--- src/components/c-picker/index.js
+++ src/components/c-picker/index.js
@@ -1,4 +1,3 @@
-import moment from 'moment';
 import DatePicker from './pickers/date';
 import TimePicker from './pickers/time';
 import YearPicker from './pickers/year';
@@ -6,8 +5,6 @@ import MonthPicker from './pickers/month';
 import DateRangePicker from './pickers/date-range';
 import TimeRangePicker from './pickers/time-range';
 import './index.less';
-
-moment.locale('zh_CN');
 
 const CPicker = DatePicker;
 
