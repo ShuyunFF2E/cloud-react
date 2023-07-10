680afeaf3afeb4d5a50ac7e768a13ca984109511
张鹏博 <pengbo.zhang@shuyun.com>
2023-07-10 17:06:39 +0800

    feat: 添加周选择器

--- src/components/c-picker/index.js
+++ src/components/c-picker/index.js
@@ -3,6 +3,7 @@ import DatePicker from './pickers/date';
 import TimePicker from './pickers/time';
 import YearPicker from './pickers/year';
 import MonthPicker from './pickers/month';
+import WeekPicker from './pickers/week';
 import DateRangePicker from './pickers/date-range';
 import TimeRangePicker from './pickers/time-range';
 import './index.less';
@@ -14,6 +15,7 @@ const CPicker = DatePicker;
 CPicker.TimePicker = TimePicker;
 CPicker.YearPicker = YearPicker;
 CPicker.MonthPicker = MonthPicker;
+CPicker.WeekPicker = WeekPicker;
 CPicker.RangePicker = DateRangePicker;
 CPicker.TimeRangePicker = TimeRangePicker;
 
---
+++ src/components/c-picker/pickers/week.js
@@ -0,0 +1,147 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import moment from 'moment';
+import momentGenerateConfig from 'rc-picker/lib/generate/moment';
+import generatePicker from '../generator';
+import { weekFormat } from '../formats';
+
+const { WeekPicker: Picker } = generatePicker(momentGenerateConfig);
+
+const WeekPicker = ({
+  size,
+  className,
+  dropdownClassName, // New
+  disabled,
+  defaultValue: _defaultValue,
+  value: _value,
+  open,
+  onOpenChange, // New
+  placeholder,
+  width,
+  minYear,
+  maxYear,
+  format: _format,
+  onChange,
+  getPopupContainer: _getPopupContainer, // New
+  isAppendToBody,
+  canEdit = true,
+  style,
+  showToday,
+  disabledDate: _disabledDate,
+  renderExtraFooter,
+  autoFocus,
+  allowClear,
+  onFocus,
+  onBlur,
+  onMouseDown,
+  onMouseUp,
+  onMouseEnter,
+  onMouseLeave,
+  onClick,
+  onContextMenu,
+  onKeyDown,
+  onSelect,
+  onPanelChange,
+}) => {
+  const [value, setValue] = useState();
+  const format = _format || weekFormat;
+
+  useEffect(() => {
+    setValue(_value && moment(_value, format).startOf('week'));
+  }, [_value, format]);
+
+  const handleChange = useCallback(
+    (m) => {
+      if (onChange) {
+        onChange(m && m.clone().format(format));
+      } else {
+        setValue(m);
+      }
+    },
+    [onChange, format],
+  );
+
+  const handleSelect = useCallback(
+    (m) => {
+      if (onSelect) {
+        onSelect(m && m.clone().format(format));
+      }
+    },
+    [onSelect, format],
+  );
+
+  const handlePanelChange = useCallback(
+    (val, mode) => {
+      if (onPanelChange) {
+        onPanelChange(val && val.clone().format(format), mode);
+      }
+    },
+    [onPanelChange, format],
+  );
+
+  const getPopupContainer = useMemo(() => {
+    if (_getPopupContainer) {
+      return _getPopupContainer;
+    }
+    if (!isAppendToBody) {
+      return () => document.body;
+    }
+    return undefined;
+  }, [_getPopupContainer, isAppendToBody]);
+
+  const getDisabledDate = useCallback(
+    (d) => {
+      const current = d.clone();
+      return (minYear && current.year() < minYear) || (maxYear && current.year() > maxYear);
+    },
+    [format, minYear, maxYear],
+  );
+
+  const handleGetDisabledDate = useCallback(
+    (m) => {
+      if (_disabledDate) {
+        return _disabledDate(m && m.clone().format(format));
+      }
+      return getDisabledDate(m);
+    },
+    [_disabledDate, getDisabledDate, format],
+  );
+
+  return (
+    <Picker
+      size={size}
+      style={{ width, ...style }}
+      defaultValue={_defaultValue && moment().year(Number(_defaultValue))}
+      onChange={handleChange}
+      onSelect={handleSelect}
+      onPanelChange={handlePanelChange}
+      inputReadOnly={!canEdit}
+      getPopupContainer={getPopupContainer}
+      disabledDate={handleGetDisabledDate}
+      {...{
+        className,
+        dropdownClassName,
+        format,
+        value,
+        disabled,
+        open,
+        placeholder,
+        showToday,
+        renderExtraFooter,
+        autoFocus,
+        allowClear,
+        onFocus,
+        onBlur,
+        onOpenChange,
+        onMouseDown,
+        onMouseUp,
+        onMouseEnter,
+        onMouseLeave,
+        onClick,
+        onContextMenu,
+        onKeyDown,
+      }}
+    />
+  );
+};
+
+export default WeekPicker;
