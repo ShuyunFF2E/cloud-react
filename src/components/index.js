7a275ac3471089f91f91b5ba5d0ea95c69c0d6ba
liyuan-meng <liyuan.meng@shuyun.com>
2024-04-17 15:03:27 +0800

    :bug:解决报错

--- src/components/index.js
+++ src/components/index.js
@@ -69,7 +69,4 @@ export { default as TreeSelect } from './tree-select';
 
 export { default as Upload } from './upload';
 
-<<<<<<< Updated upstream
 export { default as Cascade } from './cascade';
-=======
->>>>>>> Stashed changes
