9d26207a4202a213a1cc2fe2adc691c4791be4ca
jiaojiao.diao <jiaojiao.diao@shuyun.com>
2025-03-07 10:59:50 +0800

    feat: 打包测试

--- src/components/c-table/js/columnConfig.js
+++ src/components/c-table/js/columnConfig.js
@@ -192,7 +192,7 @@ class ColumnConfig extends Component {
                   }
                   this.setState({visible: false});
                   Message.success('恢复默认成功');
-                  console.log('test');
+                  console.log('test1');
                 },
                 onClose: () => {
                   this.setState({visible: true});
